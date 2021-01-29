063f4291a2c432ae4b00a0818acce77bba19bb3b
kaoko <kaoko28@gmail.com>
2021-01-29 11:18:37 -0500

    intro edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,25 +33,16 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+          Hello my name is Anthony Boston I am developer located in the Atlanta area 
+          I am a graduate of the CodeBridge program at Per Scholas and the Web Development Immersive at General Assembly here in Atlanta.
+           The opportunity to learn to program has been a dream come true for me. As a former, 
+           I was ecstatic to learn the similarities in diagnosing opponents tape and debugging code As a working professional, 
+           I found myself at a crossroads in my life where I was either going to work a job that I would just use to sustain myself or follow my passion and become a developer.
+            I have found the world of web development to be this amazing place to learn and meet amazing people that have the same fervor growth and collaboration. 
+            I hope to meet work and learn from as many people as possible!
+           Please feel free to connect with many on any of my social media sites!
           </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
-          </p>
+          
           {close}
         </article>
 
